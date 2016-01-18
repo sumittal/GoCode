f80c361742e2ac6f0d814741c1cecd29b631613d
Sumit Mittal <sumitm@one.com>
2016-01-18 11:25:45 +0530

    completed sieve.go

--- sieve.go
+++ sieve.go
@@ -4,43 +4,51 @@ import "fmt"
 
 // Send the sequence 2, 3, 4, ... to returned channel
 func generate() chan int {
-    ch := make(chan int)
-    go func() {
+	ch := make(chan int)
+	go func() {
 
-        for i := 2; ; i++ {
+		for i := 2; ; i++ {
 
-            ch <- i
-        }
-    }()
-    return ch
+			ch <- i
+		}
+	}()
+	return ch
 }
 
 // Filter out input values divisible by prime, send rest to returned channel
 func filter(in chan int, prime int) chan int {
-    out := make(chan int)
-    go func() {
+	out := make(chan int)
+	go func() {
 
-        for {
+		for {
 
-            if i := <-in; i%prime != 0 {
+			if i := <-in; i%prime != 0 {
 
-                out <- i
-            }
-        }
-    }()
-    return out
+				out <- i
+			}
+		}
+	}()
+	return out
 }
 
 func sieve() chan int {
-    out := make(chan int)
-    go func() {
-        ch := generate()
-        for {
-            prime := <-ch
-            ch = filter(ch, prime)
-            out <- prime
+	out := make(chan int)
+	go func() {
+		ch := generate()
+		for {
+			prime := <-ch
+			ch = filter(ch, prime)
+			out <- prime
 
-        }
-    }()
-    return out
+		}
+	}()
+	return out
+}
+
+func main() {
+	primes := sieve()
+	for {
+
+		fmt.Println(<-primes)
+	}
 }
